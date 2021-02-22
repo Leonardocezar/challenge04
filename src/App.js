4fb6098f44b2b2cca570538ae7dcb924639a2ad3
Leonardo Cezar dos Santos <leonardocezardossantos@MacBook-Pro-de-Leonardo.local>
2021-02-21 21:14:36 -0300

    End Challenge 4

--- src/App.js
+++ src/App.js
@@ -54,17 +54,15 @@ export default function App() {
               <View style={styles.likesContainer}>
                 <Text
                   style={styles.likeText}
-                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
                   testID={`repository-likes-${repository.id}`}
                 >
-                  {repository.likes}
+                  {repository.likes} curtidas
                 </Text>
               </View>
 
               <TouchableOpacity
                 style={styles.button}
                 onPress={() => handleLikeRepository(repository.id)}
-                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
                 testID={`like-button-${repository.id}`}
               >
                 <Text style={styles.buttonText}>Curtir</Text>
